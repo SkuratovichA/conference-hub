20a1b0a66539bf62b4b42545eb469a89543f5d8a
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2022-10-21 00:47:54 +0200

    start developing search for confs/users. add basic page & post,get

--- ch/views/searchview.py
+++ ch/views/searchview.py
@@ -1,17 +1,19 @@
+from django.shortcuts import render
 from django.views import generic
-from conferences import models
-from users import models
+from conferences import models as conferences_models
+from users import models as users_models
 import logging
 
 
 logger = logging.getLogger(__name__)
 
 
+# FIXME: change from ListView to another view to get rid of object_list
 class SearchView(generic.ListView):
-    model = models.ConferenceUserModel
+    # model = users_models.ConferenceUserModel
     template_name = 'ch/search.html'
     paginate_by = 50
-    object_list = models.ConferenceUserModel.objects.all()
+    object_list = users_models.ConferenceUserModel.objects.all()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -22,5 +24,18 @@ class SearchView(generic.ListView):
         logger.debug(args)
         logger.debug(kwargs)
         logger.debug(request)
-        context = {'object_list': models.ConferenceUserModel.objects.all()}
+        context = {'object_list': users_models.ConferenceUserModel.objects.all()}
         return super().render_to_response(context)
+
+    def get(self, request, *args, **kwargs):
+        _models = {
+            'researchers': users_models.ResearcherModel,
+            'organization': users_models.OrganizationModel
+        }
+        type = request.GET.get('type', default='researchers')
+        name = request.GET.get('name', default='')
+
+        model = _models['type']
+        # TODO create complex filter to filter by more parameters ...
+        context = {'object_list': model.objects.all().filter(name=name)}
+        return render(request, self.template_name, context)
