284208866c4af3edbbd64b4d496277a42efb1763
Dziyana Khrystsiuk <dziyana.khrystsiuk@gooddata.com>
2022-10-20 10:55:48 +0200

    Refactor to pyp8 codestyle

--- ch/views.py
+++ ch/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import redirect
 from django.utils import timezone
 from django.views import generic
-from django.shortcuts import redirect
-from ch.models import Conference, Event
+
 from ch.forms.conference import CreateConferenceForm, CreateEventForm
+from ch.models import Conference, Event
+
 
 class IndexView(generic.TemplateView):
     template_name = 'ch/index.html'
@@ -37,12 +39,13 @@ class SearchConferencesView(generic.ListView):
         """Return the future events"""
         return Conference.objects.filter(
             date_from__gte=timezone.now()
-            ).order_by('date_from')
+        ).order_by('date_from')
 
 
 class ConfInfoView(generic.DetailView):
     model = Conference
     template_name = 'ch/conference_info.html'
+
 
 class EventInfoView(generic.DetailView):
     model = Event
@@ -51,16 +54,15 @@ class EventInfoView(generic.DetailView):
 
 class CreateEventView(generic.CreateView):
     model = Event
-    form_class = CreateEventForm #todo
+    form_class = CreateEventForm  # todo
     template_name = 'ch/create_event.html'
 
     def form_valid(self, form):
         event = form.save()
-        return redirect('ch:conf_search')#todo redirect back to conf detail
+        return redirect('ch:conf_search')  # todo redirect back to conf detail
 
     def form_invalid(self, form):
         return super().form_invalid(form)
-
 
 
 class UsersAndOrganizations(generic.TemplateView):
