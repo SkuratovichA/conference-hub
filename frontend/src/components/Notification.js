0576d66b1e99eb2c83be085aba58d9ad031c917e
Dziyana Khrystsiuk <dziyana.khrystsiuk@gooddata.com>
2023-01-11 01:08:23 +0100

    edit member status indicator

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Grid, IconButton, Paper, Tooltip, Typography} from "@mui/material";
 import Box from "@mui/joy/Box";
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
-import MarkEmailUnreadIcon from '@mui/icons-material/MarkEmailUnread';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 
 export const NotificationItem = ({invite}) => {
@@ -44,22 +42,39 @@ export const NotificationItem = ({invite}) => {
 };
 
 export const MemberItem = ({member}) => {
+  let [status, setStatus] = useState({color:"info.main", text: "Pending"})
+
+  useEffect(()=>{
+      if (member.approved){
+        setStatus({color: "success.main",text:"Approved"})
+      }
+      else if (member.rejected){
+        setStatus({color: "error.main",text:"Rejected"})
+      }
+    }
+
+  ,[member])
+
+
+
     return (
         <Paper sx={{ width: "100%", fullWidth: true }} elevation={3}>
-             <Grid container>
-                 <Grid item md={10} px={2} py={2}>
+             <Grid container direction="row" justifyContent="space-between" alignItems="center">
+                 <Grid item md={8} px={2} py={2}>
 
                       <Typography gutterBottom variant="h5">
                           {member.res_name} {member.res_surname}
-                      </Typography>
-                      <Typography variant="body1" color="text.primary">
-                          Status: {member.approved ? "Approved" : member.rejected? "Rejected" : "Pending"}
                       </Typography>
                      <Typography variant="body2"  color="text.secondary">
                         Membership request send on: {member.date_sent}
                      </Typography>
 
                  </Grid>
+               <Grid item md={3} px={2} py={2}>
+                 <Typography variant="button" color={status.color}>
+                   Status: <strong>{status.text}</strong>
+                 </Typography>
+               </Grid>
              </Grid>
         </Paper>
     )
